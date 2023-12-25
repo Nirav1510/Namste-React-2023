1efde1d0dca30a4d2c7cb6b29e74f787d910e8a9
Nirav Gusai <gusainirav1@gmail.com>
2023-12-25 23:00:54 +0530

    Food ordering app initial commit

--- App.js
+++ App.js
@@ -1,9 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Namste React");
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logo-container'>
+        <img
+          className='logo'
+          src='https://png.pngtree.com/template/20210519/ourmid/pngtree-food-delivery-logo-fitness-template-image_525748.png'
+        />
+      </div>
+      <div className='nav-items'>
+        <ul className=''>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-console.log(heading);
+const RestaurantCard = () => {
+  return (
+    <div className='res-card' style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        alt='icon'
+        className='res-logo'
+        src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/c627e55c996978a070c10079ae57f706'
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38mins</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className='body'>
+      <div className='search'>Search</div>
+      <div className='res-container'>
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<AppLayout />);
